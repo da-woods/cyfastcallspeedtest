f1439ef0165498eda954249803abddee53ce42da
da-woods <dw-git@d-woods.co.uk>
2020-02-06 13:21:18 +0000

    Fix keyword names

--- fastcall_speedtest.py
+++ fastcall_speedtest.py
@@ -9,9 +9,9 @@ for func_name in ('not_fastcall', 'should_be_fastcall',
     number = 10000000
     args = "10000,2,3,4" if func_name.find("kwds")==-1 else "a=1,b=2,c=3,d=4"
     if func_name == "forward_call_kwds":
-        args = "10000,"+args
+        args = "10000,w=1,x=2,y=3,z=4"
     elif func_name == "forward_call_both":
-        args = "10000,2,3,4,a=1,b=2,c=3,d=4"
+        args = "10000,2,3,4,w=1,x=2,y=3,z=4"
     if func_name.startswith("forward_call"):
         number = 1000
     print("Without", timeit("{0}({1})".format(func_name, args),
